38b2681f8675f80a219c58c5013d463ee0a03770
Thiago Martins <thiagocmartinsc@gmail.com>
2019-03-11 08:49:11 -0400

    Renaming hyper_virtualbox to just virtualbox

--- scripts/preset-svauto-desktop.sh
+++ scripts/preset-svauto-desktop.sh
@@ -2,4 +2,4 @@
 
 ./svauto.sh --ansible-remote-user="root" \
 	--ansible-inventory-builder="all,localhost,ansible_connection=local,mode=desktop,os_release=rocky" \
-	--ansible-playbook-builder="localhost,bootstrap;base_os_upgrade=yes,grub-conf,hyper_kvm,hyper_lxd,hyper_virtualbox,docker,vagrant,amazon_ec2_tools,redhat_tools_ubuntu,os_clients,packer,vsftpd,download-images;download_group=iso;download_ubuntu=yes,post-cleanup"
+	--ansible-playbook-builder="localhost,bootstrap;base_os_upgrade=yes,grub-conf,hyper_kvm,hyper_lxd,virtualbox,docker,vagrant,amazon_ec2_tools,redhat_tools_ubuntu,os_clients,packer,vsftpd,download-images;download_group=iso;download_ubuntu=yes,post-cleanup"
