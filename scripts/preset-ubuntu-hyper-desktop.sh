f3882e45437189f022a0f52f1498d6cb9585773e
Thiago Martins <thiagocmartinsc@gmail.com>
2019-03-19 09:29:51 -0400

    Renaming hyper_lxd to new lxd at the scripts

--- scripts/preset-ubuntu-hyper-desktop.sh
+++ scripts/preset-ubuntu-hyper-desktop.sh
@@ -2,4 +2,4 @@
 
 ./svauto.sh \
 	--ansible-inventory-builder="all,localhost,ansible_connection=local,mode=desktop,os_release=rocky" \
-        --ansible-playbook-builder="localhost,bootstrap;base_os_upgrade=true,grub,ubuntu-desktop,ssh_keypair,vagrant,os_clients,google-chrome,ubuntu-virt;setup=host,hyper_lxd,virtualbox,download-images;download_group=iso;download_ubuntu=true,post-cleanup"
+        --ansible-playbook-builder="localhost,bootstrap;base_os_upgrade=true,grub,ubuntu-desktop,ssh_keypair,vagrant,os_clients,google-chrome,ubuntu-virt;setup=host,lxd;setup=host,virtualbox,download-images;download_group=iso;download_ubuntu=true,post-cleanup"
--- scripts/preset-ubuntu-hyper-openvswitch-server.sh
+++ scripts/preset-ubuntu-hyper-openvswitch-server.sh
@@ -2,4 +2,4 @@
 
 ./svauto.sh \
 	--ansible-inventory-builder="all,localhost,ansible_connection=local,mode=server,openvswitch_mode=regular,os_release=rocky" \
-	--ansible-playbook-builder="localhost,bootstrap;base_os_upgrade=true,grub,openvswitch,openvswitch_bridges,ubuntu-virt;setup=host,hyper_lxd,download-images;download_group=iso,post-cleanup"
+	--ansible-playbook-builder="localhost,bootstrap;base_os_upgrade=true,grub,openvswitch,openvswitch_bridges,ubuntu-virt;setup=host,lxd;setup=host,download-images;download_group=iso,post-cleanup"
