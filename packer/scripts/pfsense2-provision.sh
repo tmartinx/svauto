21e734a73d3289d675108a054ab0555c703f8f62
Thiago Martins <thiagocmartinsc@gmail.com>
2019-03-23 23:09:58 -0400

    New PFSense Packer Provisioning script
    We're building for KVM VirtIO Net, not for AWS.
    
    It's interesting to note that I tried to build the PFSense QCoW2 using the
    virtio-net from the start, it failed, not sure why.

--- packer/scripts/pfsense2-provision.sh
+++ packer/scripts/pfsense2-provision.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-# replace <if></if> section of config.xml with proper names for running on AWS EC2
-sed -i -E 's$<if>em0</if>$<if>xn0</if>$g' /conf/config.xml
-sed -i -E 's$<if>em1</if>$<if>xn1</if>$g' /conf/config.xml
-sed -i -E 's$<if>em0</if>$<if>xn0</if>$g' /conf.default/config.xml
-sed -i -E 's$<if>em1</if>$<if>xn1</if>$g' /conf.default/config.xml
+# replace <if></if> section of config.xml with proper names for running on KVM VirtIO Net
+echo "PROVISIONING: Changing /conf/config.xml and /conf.default/config.xml:"
+sed -i -E 's$<if>em0</if>$<if>vtnet0</if>$g' /conf/config.xml
+sed -i -E 's$<if>em1</if>$<if>vtnet1</if>$g' /conf/config.xml
+sed -i -E 's$<if>em0</if>$<if>vtnet0</if>$g' /conf.default/config.xml
+sed -i -E 's$<if>em1</if>$<if>vtnet1</if>$g' /conf.default/config.xml
 
 # print grep results for verification during packer build:
-echo "grep -H <if> ... output:"
+echo "PROVISIONING: grep -H <if> ... output:"
 grep -H '<if>' /conf/config.xml
 grep -H '<if>' /conf.default/config.xml
