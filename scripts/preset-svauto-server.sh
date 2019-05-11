1304c3433161ab16781ee4d225857d10a754560c
Thiago Martins <thiagocmartinsc@gmail.com>
2019-05-11 02:09:51 +0200

    New download-images role
    It's more dynamic now, and uses Ansible stat module.

--- scripts/preset-svauto-server.sh
+++ scripts/preset-svauto-server.sh
@@ -2,4 +2,4 @@
 
 ./svauto.sh \
 	--ansible-inventory-builder="all,localhost,ansible_connection=local,mode=server,os_release=rocky" \
-	--ansible-playbook-builder="localhost,bootstrap;base_os_upgrade=true,grub,apache2,ubuntu-virt;setup=host,lxd;setup=host,virtualbox,docker,vagrant,amazon_ec2_tools,redhat_tools_ubuntu,os_clients,packer,vsftpd,download-images;download_group=iso;download_ubuntu=true,post-cleanup"
+	--ansible-playbook-builder="localhost,bootstrap;base_os_upgrade=true,grub,apache2,ubuntu-virt;setup=host,lxd;setup=host,virtualbox,docker,vagrant,amazon_ec2_tools,redhat_tools_ubuntu,os_clients,packer,vsftpd,download-images,post-cleanup"
